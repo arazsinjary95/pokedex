c14e42c6ceba311794f3f4ac62a65c4ae7f08f31
Araz Sinjary <araz@Arazs-MBP.Home>
2018-08-03 18:15:26 +0300

    install alamofire

--- pokedex/Controller/PokemobDetailVC.swift
+++ pokedex/Controller/PokemobDetailVC.swift
@@ -11,6 +11,17 @@ import UIKit
 class PokemobDetailVC: UIViewController {
 
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     
     var pokemon: Pokemon!
     
@@ -20,4 +31,8 @@ class PokemobDetailVC: UIViewController {
         nameLbl.text = pokemon.name
     }
 
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
